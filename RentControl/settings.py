545ef1990e1787bf433a778b0da57133aa773941
Juan David <juandavidgodoy@hotmail.com>
2024-11-15 18:45:05 -0500

    tests selenium

--- RentControl/settings.py
+++ RentControl/settings.py
@@ -84,12 +84,11 @@ import os
 load_dotenv()
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default= os.getenv('DATABASE_URL'),
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
--- Usuarios/tests.py
+++ Usuarios/tests.py
@@ -1,3 +1,45 @@
-from django.test import TestCase
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 
-# Create your tests here.
+class MySeleniumTests(LiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.browser = webdriver.Chrome() 
+        cls.browser.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.browser.quit()
+        super().tearDownClass()
+
+    def test_register_and_login(self):
+        self.browser.get(f"{self.live_server_url}/signup/")
+
+        username_input = self.browser.find_element(By.NAME, "username")
+        password1_input = self.browser.find_element(By.NAME, "password1")
+        password2_input = self.browser.find_element(By.NAME, "password2")
+
+        username_input.send_keys("Pablo")
+        password1_input.send_keys("pablo")
+        password2_input.send_keys("pablo")
+        
+        password2_input.send_keys(Keys.RETURN)
+        
+        time.sleep(1) 
+        
+        self.browser.get(f"{self.live_server_url}/logout/")
+        self.browser.get(f"{self.live_server_url}/signin/")
+
+        username_input = self.browser.find_element(By.NAME, "username")
+        password_input = self.browser.find_element(By.NAME, "password")
+
+        username_input.send_keys("Pablo")
+        password_input.send_keys("pablo")
+        
+        password_input.send_keys(Keys.RETURN)
+
+        time.sleep(1)
